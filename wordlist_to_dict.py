8334a04d19ad351de302ee5f0d463570657017a4
Shtinoeh <s113101@student.dtu.dk>
2014-10-27 10:25:58 +0100

    Made it a module

--- wordlist_to_dict.py
+++ wordlist_to_dict.py
@@ -1,14 +1,16 @@
 import codecs
-
 word_dict = {}
-s_line = []
 
-wordlist = codecs.open("FINN-wordlist.txt", "r",encoding='utf8')
-for line in wordlist:
-    s_line.append(line.split('\t'))
-wordlist.close()
+def wordlist_to_dict(list):
+    
+    s_line = []
+    wordlist = codecs.open(list, "r",encoding='utf8')
+    for line in wordlist:
+        s_line.append(line.split('\t'))
+    wordlist.close()
 
-for w,n in s_line:
-    word_dict[w] = n
+    for w,n in s_line:
+        word_dict[w] = n
 
+#wordlist_to_dict("FINN-wordlist.txt")
 #print word_dict['limited']
