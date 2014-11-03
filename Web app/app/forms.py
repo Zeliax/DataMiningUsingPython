9c25ebe99389f7cf667eed295636aaa359367466
MetteMH <mettemaihansen@gmail.com>
2014-11-03 18:51:44 +0100

    Changed web app

---
+++ Web app/app/forms.py
@@ -0,0 +1,6 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class SearchForm(Form):
+    search_word = StringField('search_word', validators=[DataRequired()])
---
+++ Web app/app/views.py
@@ -0,0 +1,33 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import SearchForm
+
+@app.route('/')
+# @app.route('/index')
+# def index():
+#     user = {'nickname': 'Mette'}  # fake user
+#     posts = [  # fake array of posts
+#         { 
+#             'author': {'nickname': 'John'}, 
+#             'body': 'Beautiful day in Portland!' 
+#         },
+#         { 
+#             'author': {'nickname': 'Susan'}, 
+#             'body': 'The Avengers movie was so cool!' 
+#         }
+#     ]
+#     return render_template('index.html',
+#                            title='Youtube Sentiment Analysis',
+#                            user=user,
+#                            posts=posts)
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        flash('Search requested for search_word="%s"' %
+              (form.search_word.data))
+        return redirect('templates/index')
+    return render_template('search.html', 
+                           title='Search',
+                           form=form)
