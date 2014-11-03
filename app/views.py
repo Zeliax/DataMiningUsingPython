2bb62945ffec08102c67eee2de0ccd4294376aa7
Zeliax <Thomashammerbak@gmail.com>
2014-11-03 14:40:52 +0100

    Update gitignore and folder structure

--- app/views.py
+++ app/views.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
