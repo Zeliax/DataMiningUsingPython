c85d12f1116f7e8b4c1ca4799dff74a3b175683d
Shtinoeh <s113101@student.dtu.dk>
2014-11-08 16:45:38 +0100

    Added return

--- Web app/app/scripts/wordlist_to_dict.py
+++ Web app/app/scripts/wordlist_to_dict.py
@@ -2,8 +2,7 @@ import codecs
 word_dict = {}
 
 def wordlist_to_dict(list):
-	"""Creates a dictionary from a wordlist"""
-    
+    """Creates a dictionary from a wordlist"""
     s_line = []
     wordlist = codecs.open(list, "r",encoding='utf8')
     for line in wordlist:
@@ -12,6 +11,6 @@ def wordlist_to_dict(list):
 
     for w,n in s_line:
         word_dict[w] = n
-
+    return word_dict
 #wordlist_to_dict("FINN-wordlist.txt")
 #print word_dict['limited']
