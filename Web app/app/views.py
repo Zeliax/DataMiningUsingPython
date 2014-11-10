d49bb48e966b54bc0082040f5438f56fc66ac220
MetteMH <mettemaihansen@gmail.com>
2014-11-10 16:28:29 +0100

    Changed YouTubeConnection + web app

--- Web app/app/views.py
+++ Web app/app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect
 from app import app
 from .forms import SearchForm
-from .YouTubeVideoComments import main
+from YouTubeConnection import main_func
 
 @app.route('/') #URL-path to homepage
 @app.route('/index')
@@ -22,13 +22,22 @@ def comment():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
+    comment_list = []
+    # table = []
     if form.validate_on_submit():
+        search_word = form.search_word.data
+        nr_of_results = 1
+        comment_list = main_func(search_word, nr_of_results)
+        assert comment_list
+        # for video_id, comment in comment_dict.iteritems():
+        #     temp = []
+        #     temp.extend([video_id])  #Note that this will change depending on the structure of your dictionary
+        #     table.append(temp)
         flash('Search requested for "%s"' %
-              (form.search_word.data))
-        return redirect('/search')
-
-    search_word = form.search_word.data
-    nr_of_results = 3
-    main_func(search_word, nr_of_results)
+        (search_word))
+        return render_template('search.html',
+                           form=form,
+                           comment_list=comment_list)
     return render_template('search.html',
-                           form=form)
+                           form=form,
+                           comment_list=comment_list)
