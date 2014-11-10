9edaff471bf8dfaea307d64ea6ebf896c4e39f8b
Shtinoeh <s113101@student.dtu.dk>
2014-11-10 15:48:28 +0100

    Changed to class

--- Web app/app/scripts/detect_lang.py
+++ Web app/app/scripts/detect_lang.py
@@ -2,24 +2,24 @@ from nltk.corpus import stopwords   # stopwords to detect language
 from nltk import wordpunct_tokenize  # function to split up our words
 
 
-def get_language_likelihood(input_text):
-    """Return a dictionary of languages and their likelihood of being the
-    language of the comment.
-    """
+class LanguageDetector(object):
+    """docstring for ClassName"""
+    def __init__(self):
+        self.lang_stopwords = {language: set(stopwords.words(language))
+                               for language in stopwords._fileids}
 
-    input_text = input_text.lower()
-    input_words = wordpunct_tokenize(input_text)
+    def get_language_likelihood(self, input_text):
+        """Return a dictionary of languages and their likelihood of being the
+        language of the comment.
+        """
+        input_text = input_text.lower()
+        input_words = wordpunct_tokenize(input_text)
+        language_likelihood = {}
+        for language, stopword_set in self.lang_stopwords.items():
+            language_likelihood[language] = len(set(input_words) & stopword_set)
+        return language_likelihood
 
-    language_likelihood = {}
-    for language in stopwords._fileids:
-        language_likelihood[language] = len(set(input_words) &
-                                            set(stopwords.words(language)))
-
-    return language_likelihood
-
-
-def get_language(input_text):
-    """Return the most likely language of the given comment"""
-
-    likelihoods = get_language_likelihood(input_text)
-    return sorted(likelihoods, key=likelihoods.get, reverse=True)[0]
+    def get_language(self, input_text):
+        """Return the most likely language of the given comment"""
+        likelihoods = self.get_language_likelihood(input_text)
+        return sorted(likelihoods, key=likelihoods.get, reverse=True)[0]
