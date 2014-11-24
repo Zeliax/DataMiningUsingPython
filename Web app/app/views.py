ec19cf751771eeab20b93d5ec3d873e2404c7eb2
MetteMH <mettemaihansen@gmail.com>
2014-11-24 13:44:35 +0100

    Web app

--- Web app/app/views.py
+++ Web app/app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, request
+from flask import render_template, flash, redirect, request, make_response
 from app import app
 from .forms import SearchForm
 from YouTubeConnection import main_func
@@ -51,3 +51,33 @@ def search():
                            links=links,
                            zipped=zipped,
                            pos_neg=pos_neg)
+
+@app.route("/simple.png")
+def simple():
+    import datetime
+    import StringIO
+    import random
+ 
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+ 
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = StringIO.StringIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
