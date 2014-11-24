eaf97a52c6352ae4cd9f678a97194a3d1b096231
Shtinoeh <s113101@student.dtu.dk>
2014-11-24 12:45:54 +0100

    Corrected grammar

--- Web app/app/views.py
+++ Web app/app/views.py
@@ -5,14 +5,16 @@ from YouTubeConnection import main_func
 from sentimentAnalysis import sentiment_analysis, wordlist_to_dict, sentiment
 from Plotter import pie_chart, pos_neg_counter
 
-@app.route('/') #URL-path to homepage
+
+@app.route('/')  # URL-path to homepage
 @app.route('/index')
 def index():
-	title='Youtube Sentiment Analysis' #setting the title
-	paragraph = "Welcome!"
-	return render_template('index.html',
+    title = 'Youtube Sentiment Analysis'  # setting the title
+    paragraph = "Welcome!"
+    return render_template('index.html',
                            title=title,
                            paragraph=paragraph)
+
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -23,27 +25,25 @@ def search():
     links = []
     zipped = []
     pos_neg = []
-    word_dict = wordlist_to_dict(r'C:\Users\Mette\Documents\GitHub\DataMiningUsingPython\Web app\app\FINN-wordlist.txt')
+    word_dict = wordlist_to_dict()
     if form.validate_on_submit():
-        nr_of_results = 1
         search_word = request.form['search_word']
         nr_of_results = request.form['nr_of_results']
         commentlist, names, links = main_func(search_word, nr_of_results)
         assert commentlist
-        # sentiment = sentiment_analysis(commentlist,word_dict)
-        # zipped = zip(commentlist, sentiment)
-        # pos_neg = pos_neg_counter(sentiment)
+        sentiment = sentiment_analysis(commentlist, word_dict)
+        zipped = zip(commentlist, sentiment)
+        pos_neg = pos_neg_counter(sentiment)
         # pieChart = pie_chart(pos_neg)
-        flash('Search requested for "%s"' %
-        (search_word))
+        flash('Search requested for "%s"' % (search_word))
         return render_template('search.html',
-                           form=form,
-                           sentiment=sentiment,
-                           commentlist=commentlist,
-                           names=names,
-                           links=links,
-                           zipped=zipped,
-                           pos_neg=pos_neg)
+                               form=form,
+                               sentiment=sentiment,
+                               commentlist=commentlist,
+                               names=names,
+                               links=links,
+                               zipped=zipped,
+                               pos_neg=pos_neg)
     return render_template('search.html',
                            form=form,
                            sentiment=sentiment,
