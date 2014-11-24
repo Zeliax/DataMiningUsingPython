f7bcf392861d45edd5eeeded0ad62a90d249fac2
Shtinoeh <s113101@student.dtu.dk>
2014-11-24 17:00:21 +0100

    Added test and fixed sentiment_analysis

--- Web app/app/sentimentAnalysis.py
+++ Web app/app/sentimentAnalysis.py
@@ -11,26 +11,12 @@ def wordlist_to_dict():
     LIST = codecs.open(path + "\\app\\FINN-wordlist.txt", "r", encoding='utf8')
     word_dict = {}
     s_line = []
-    wordlist = LIST
-    for line in wordlist:
+    for line in LIST:
         s_line.append(line.split('\t'))
-    wordlist.close()
-    for w, n in s_line:
-        word_dict[w] = n
+    LIST.close()
+    for word, sentiment in s_line:
+        word_dict[word] = int(sentiment)
     return word_dict
-
-#wordlist_to_dict("FINN-wordlist.txt")
-#print word_dict['limited']
-
-
-def comments_to_list(textfile):
-    """Creates a list of comments from a textfile"""
-    commentlist = []
-    comments = open(textfile, "r")
-    for line in comments:
-        commentlist.append(line)
-    comments.close()
-    return commentlist
 
 
 def sentiment(words, word_dict):
@@ -39,11 +25,11 @@ def sentiment(words, word_dict):
     sent_values = []
     for word in words:
         if word in word_dict:
-            sent_values.append(int(word_dict[word]))
+            sent_values.append(word_dict[word])
             #print "The word is %s and the count is %d" %(word, count)
         else:
             #print "Word %s passed" %word
-            pass
+             sent_values.append(0)
     # print "Total count is: %r" % count
     return sent_values
 
@@ -52,7 +38,7 @@ def sentiment_analysis(commentlist, wordlist):
     """Calculates the mean sentiment of each comment.
 
     Keyword arguments:
-    commentlist -- a list of comments
+    commentlist -- a list of lists of comments
     """
     #total_sentiment = 0
     tokenizer = RegexpTokenizer(r'[a-z]+')
@@ -67,6 +53,4 @@ def sentiment_analysis(commentlist, wordlist):
                 words = tokenizer.tokenize(comment)
                 #sent_sentiment is a list of sentiments for each comment
                 sent_sentiment.append(np.mean(sentiment(words, wordlist)))
-            else:
-                pass
     return sent_sentiment
