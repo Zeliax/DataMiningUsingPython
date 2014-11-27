1349be5a1c7da9f48d1b3cb70a814edf649ba8b5
Zeliax <Thomashammerbak@gmail.com>
2014-11-27 14:29:34 +0100

    Plotter changed

--- Web app/app/Plotter.py
+++ Web app/app/Plotter.py
@@ -4,8 +4,10 @@ import matplotlib
 import random
 import numpy
 
+# from time import sleep
 
-def pie_chart(fraction):
+
+def pie_chart(fractions_list):
     """
     Function for plotting a piechart of sentiment analysis of YouTube videos.
 
@@ -19,9 +21,20 @@ def pie_chart(fraction):
     matplotlib.rcParams['patch.edgecolor'] = 'white'
     matplotlib.rcParams['font.style'] = 'oblique'
     matplotlib.rcParams['font.size'] = 18
-    plot = plt.figure(1, figsize=(10, 10))
-    plt.pie(fraction, labels=labels, colors=colors, autopct='%.0f%%')
-    return plot
+
+    # for fraction in fractions_list:
+    #     plt.subplots(2, figsize=(10, 10))
+    #     plt.pie(fraction, labels=labels, colors=colors)
+    # plot.show()
+
+    fig = plt.figure()
+    for i in xrange(1, 3):
+        plt.subplot(220 + i)
+        plt.pie(fractions_list[i - 1], labels=labels, autopct='%1.1f%%',
+                colors=colors)
+
+    fig.subplots_adjust(hspace=1)
+    return fig
 
 
 def hist_graph():
@@ -31,8 +44,7 @@ def hist_graph():
 def pos_neg_counter(sent_list):
     pos = len([sent for sent in sent_list if sent >= 6 and sent <= 12])
     neg = len([sent for sent in sent_list if sent < 6 and sent >= 0])
-    fraction = (pos, neg)
-    return fraction
+    return (pos, neg)
 
 
 def list_divider(list_list):
@@ -40,8 +52,7 @@ def list_divider(list_list):
 
 
 def generate_subplot_2d(plot_list, plot_names):
-    for plot in plot_list:
-        plot.show()
+    print "Hello"
 
 
 def main():
@@ -49,17 +60,58 @@ def main():
     sent_score1 = [random.randrange(0, 12, 1) for nr in scores1]
     scores2 = numpy.ones(50)
     sent_score2 = [random.randrange(0, 12, 1) for nr in scores2]
+    # scores3 = numpy.ones(50)
+    # sent_score3 = [random.randrange(0, 12, 1) for nr in scores3]
+    # scores4 = numpy.ones(50)
+    # sent_score4 = [random.randrange(0, 12, 1) for nr in scores4]
+
+    # scores5 = numpy.ones(50)
+    # sent_score5 = [random.randrange(0, 12, 1) for nr in scores5]
+    # scores6 = numpy.ones(50)
+    # sent_score6 = [random.randrange(0, 12, 1) for nr in scores6]
 
     lal_list = []
     lal_list.append(sent_score1)
     lal_list.append(sent_score2)
 
     #Mette from here
-    fractions = list_divider(lal_list)
+    fractions_list = list_divider(lal_list)
 
-    for fraction in fractions:
-        fig = pie_chart(fraction)
-        fig.show()
+    print fractions_list
+
+    # for fraction in fractions_list:
+    chart = pie_chart(fractions_list)
+    chart.show()
+
+
+    # matplotlib.rcParams['text.color'] = 'white'
+    # matplotlib.rcParams['lines.linewidth'] = 2
+    # matplotlib.rcParams['patch.edgecolor'] = 'white'
+    # matplotlib.rcParams['font.style'] = 'oblique'
+    # matplotlib.rcParams['font.size'] = 18
+    # labels = ['Positive', 'Negative']
+    # colors = ('g', 'r')
+
+    # for fraction in fractions_list:
+    # fig = plt.figure()
+    # for i in xrange(1, 3):
+    #     plt.subplot(220 + i)
+    #     plt.pie(fractions_list[i - 1], labels=labels, autopct='%1.1f%%',
+    #             colors=colors)
+
+            # ax1 = fig.add_subplot(2, 1, 1)
+            # plt.pie(fraction[i], labels=labels, autopct='%1.1f%%',
+            #         colors=colors)
+            # ax2 = fig.add_subplot(2, 1, 2)
+            # plt.pie(fractions_list[i], labels=labels, autopct='%1.1f%%',
+                    # colors=colors)
+
+            # plt.subplot(220 + i)
+    # fig.subplots_adjust(hspace=1)
+
+    # plt.show()
+
+    # pie_chart(fractions_list, plot_names)
 
 if __name__ == '__main__':
     main()
